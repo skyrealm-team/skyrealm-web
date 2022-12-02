b6c1e875521f9ccd2052b83e2398747dcf118c0d
Jinquan Liu <jinquan.liu@skyrealm.ai>
2022-12-02 10:10:06 +0800

    add loader to google map

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,6 +6,7 @@ import theme from 'shared/theme';
 import { RootStoresProvider, rootStore } from 'stores/index';
 import { ApolloProvider } from '@apollo/client';
 import client from 'graphql/client';
+import Loading from 'components/Loading';
 import Nav from './Nav';
 import Router from './Router';
 import Footer from './Footer';
@@ -17,7 +18,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <RootStoresProvider value={rootStore}>
-          <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? ''} libraries={['places']}>
+          <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? ''}
+            libraries={['places']}
+            loadingElement={<Loading />}
+          >
             <BrowserRouter>
               <Stack
                 sx={{
