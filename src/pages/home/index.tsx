613bf47e16ba389bcf30b17343d3af93b61eb634
Jinquan Liu <jinquan.liu@skyrealm.ai>
2023-01-07 01:58:35 +0800

    test args

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 import { NextPage } from "next";
-import getConfig from "next/config";
 
 import { Stack } from "@mui/material";
 
@@ -12,14 +11,10 @@ import ListingsFiltersBar from "components/ListingsFiltersBar";
 import ListingsMap from "components/ListingsMap";
 import Loading from "components/Loading";
 
-const { publicRuntimeConfig } = getConfig();
-
 const libraries: LoadScriptProps["libraries"] = ["places"];
 
 const Home: NextPage = () => {
   const [hovering, setHovering] = useState<SingleListing["listingId"]>();
-
-  console.log(publicRuntimeConfig.env.NEXT_PUBLIC_BACKEND_API);
 
   return (
     <LoadScriptNext
