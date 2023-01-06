2d6a7666ece05740553d65eae496e9fc69825e60
Jinquan Liu <jinquan.liu@skyrealm.ai>
2023-01-06 23:32:15 +0800

    test env

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
     return config;
   },
   optimizeFonts: true,
-  publicRuntimeConfig: {
-    env: {
-      BACKEND_API: {
-        development: "https://dev.skyrealm.ai/gql/schema",
-        production: "https://www.skyrealm.ai/gql/schema",
-      }[process.env.NODE_ENV ?? "development"],
-    },
-  },
 };
 
 module.exports = nextConfig;
